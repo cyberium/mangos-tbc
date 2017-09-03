bc1a4b9f59d2ec9cdd2680311d4e8957d1c07307
cyberium <cyberium@users.noreply.github.com>
2017-09-03 16:04:29 +0200

    Implement instance limit per account.
    This is rewrited version of @killerwife idea/code

---
+++ sql/updates/characters/s0000_01_characters_account_instances_entered.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `account_instances_entered`;
+CREATE TABLE `account_instances_entered` (
+   `AccountId` INT(11) UNSIGNED NOT NULL COMMENT 'Player account',
+   `ExpireTime` BIGINT(40) NOT NULL COMMENT 'Time when instance was entered',
+   `InstanceId` INT(11) UNSIGNED NOT NULL COMMENT 'ID of instance entered',
+   PRIMARY KEY(`AccountId`,`InstanceId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='Instance reset limit system';
